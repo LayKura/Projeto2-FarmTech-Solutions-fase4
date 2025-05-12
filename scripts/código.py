ee61fa0f25f748679529d5bd0519e60ee0eda667
Davi Ferreira <davistudyreference@gmail.com>
2025-05-12 19:03:49 -0300

    incluso na script conexão com o BD e funções de CRUD

--- scripts/código.py
+++ scripts/código.py
@@ -1,3 +1,3 @@
 import oracledb, pandas as pd
 
-conn = oracledb.connect(user='rm562274', password='090402', dsn='ORCL')
+conn = oracledb.connect(user='rm562274', password='090402', dsn='oracle.fiap.com.br:1521/ORCL')
