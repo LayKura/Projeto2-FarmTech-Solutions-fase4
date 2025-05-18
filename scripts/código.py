98fa9c6b8eb12bb52c301cf18331d60d1dd8d8a9
Davi Ferreira <davistudyreference@gmail.com>
2025-05-18 12:56:19 -0300

    Adicionado opção de inserir dados

--- scripts/código.py
+++ scripts/código.py
@@ -1,20 +1,23 @@
-import oracledb, pandas as pd, datetime, time, random
+import oracledb, pandas as pd, datetime, os, time
 
 conn = oracledb.connect(user='rm562274', password='090402', dsn='oracle.fiap.com.br:1521/ORCL')
 cursor = conn.cursor()
 
 # Inserção de dado
 def inserir_dado(umidade, ph, fosforo, potassio, bomba):
-    timestamp = datetime.now().isoformat()
+    time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     cursor.execute('''
-    INSERT INTO sensores (timestamp, umidade, ph, fosforo, potassio, bomba)
-    VALUES (?, ?, ?, ?, ?, ?)
-    ''', (timestamp, umidade, ph, fosforo, potassio, bomba))
+    INSERT INTO sensores (time, umidade, ph, fosforo, potassio, bomba)
+    VALUES (:1, :2, :3, :4, :5, :6)
+    ''', (time, umidade, ph, fosforo, potassio, bomba))
     conn.commit()
 
 # Consulta
-def listar_dados():
-    cursor.execute('SELECT * FROM sensores')
+def listar_dados(min: str | None = ..., max:str | None = ..., date_options: bool = False):
+    if date_options == True:
+        cursor.execute(f'SELECT * FROM sensores where time between {min} and {max}')
+    else:
+        cursor.execute('SELECT * FROM sensores')
     return cursor.fetchall()
 
 # Atualizar dado
@@ -29,25 +32,74 @@ def deletar_dado(id):
     cursor.execute('DELETE FROM sensores WHERE id = ?', (id,))
     conn.commit()
 
-# Função para inserir dados simulados
-def leitura_simulada(loop):
-    df = pd.DataFrame({'data':'', 'umidade':'', 'ph':'', 'fosforo':'', 'potassio':'', 'bomba':''})
-    for _ in range(loop):
-        sensor_p = random.randint(0, 1)
-        sensor_k = random.randint(0, 1)
-        ph = round(random.uniform(4.5, 8.0), 2)
-        umidade = round(random.uniform(10.0, 80.0), 1)
-        rele_status = 'ON' if umidade < 40.0 else 'OFF'
-        data_hora = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+while True:
+    os.system('cls')
+    menu_option = int(input(f'''{'-'*15}
+Seleciona a ação que deseja realizar
 
-        print(f"[{data_hora}] Inserido: P={sensor_p}, K={sensor_k}, pH={ph}, Umidade={umidade}, Relé={rele_status}")
-        
-        # df = pd.DataFrame({'data':'', 'umidade':'', 'ph':'', 'fosforo':'', 'potassio':'', 'bomba':''})
-        
-        df['data'].add(data_hora)
+[1] Inserir dados
+[2] Consultar dados cadastrados
+[3] Atualizar dados cadastrados
+[4] Excluir dados cadastrados
+[5] Sair
+{'-'*15}
+R: '''))
+    try:
+        match menu_option:
+            case 1:
+                os.system('cls')
+                while True:
+                    try:
+                        umidade = int(input('Umidade: '))
+                        ph = int(input('PH: '))
+                        fosforo = input('Fosforo: ')
+                        potassio = int(input('Potassio: '))
+                        bomba = input('RELÉ ON ou OFF?: ')
+                        bomba = bomba.upper()
+                        os.system('cls')
+
+                        if bomba not in ['ON','OFF']:
+                            raise ValueError('O Valor da bomba está errado, apenas os valores ON e OFF são compativeis.')
+                        
+                        inserir_dado(umidade, ph, fosforo, potassio, bomba)
+                    except Exception as e:
+                        print(f'Erro:{e}')
+                        
+                    option_1 = int(input('Deseja inserir mais algum dado?:\n1-SIM\n2-NÃO\nR: '))
+                    if option_1 == 1:
+                        continue
+                    elif option_1 == 2:
+                        break
+                    else:
+                        os.system('cls')
+                        input('OPÇÃO INVALIDA: Pressione enter para voltar ao MENU')
+            
+            case 2:
+                os.system('cls')
+                try:
+                    option_2 = int(input('Deseja fazer a consulta com um range de data?\n1-SIM\n2-NÃO\nR: '))
+                    if  option_2 == 1:
+                        date_min = input('Digite a data minima que deseja pesquisar seguinte o padrão ANO-MES-DIA\nR: ')
+                        date_max = input('Digite a data maxima que deseja pesquisar seguinte o padrão ANO-MES-DIA\nR: ')
+                        df = listar_dados(date_min, date_max, True)
+                    else:
+                        df = listar_dados()
+                    os.system('cls')
+                    print(df)
+                    input('Aperte enter para voltar ao MENU')
+                ## Armazena os dados de cadastro em formato de dicionário
+                except Exception as e:
+                    os.system('cls')
+                    print(f'ERRO: {e}')
+                    time.sleep(2)
 
 
-leitura_simulada(100)
-time.sleep(1)
+            case 5:
+                conn.close()
+                exit()
 
-conn.close()
+    except Exception as error:
+        os.system('cls')
+        print(f'ERRO: {error}')
+            
+            
